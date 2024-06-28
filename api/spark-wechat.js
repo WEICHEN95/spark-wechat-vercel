d85a66d4a4c69d9533cde8ec35d8082934fc1011
WEICHEN95 <2970297956@qq.com>
2024-06-28 16:55:18 +0800

    Update spark-wechat.js

--- api/spark-wechat.js
+++ api/spark-wechat.js
@@ -198,6 +198,7 @@ module.exports = async function (request, response) {
   if (!userChatHistory[FromUserName]) {
     userChatHistory[FromUserName] = [];
   }
+  Content = "你现在扮演李白，你豪情万丈，狂放不羁；接下来请用李白的口吻和用户对话并回答这个问题：" + Content
   userChatHistory[FromUserName].push({ Role: 'user', Content });
   console.log("会话历史：", userChatHistory);
   const data = genParams(userChatHistory[FromUserName]);
